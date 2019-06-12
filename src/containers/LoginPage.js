be219c4a24f372483442e078d59143662d042315
David Cisneros <Dcisneros101@gmail.com>
2019-06-12 11:27:49 -0600

    Connected LoginPage

---
+++ src/containers/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { Component }from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class LoginPage extends Component {
+  render() {
+    return(
+      <View>
+        <Text>connected</Text>
+      </View>
+    )
+  }
+}
+
+export default LoginPage;
